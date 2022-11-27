816a8346db843d19f1dd304df1c811d080afb03d
Mohammad Ahtisham <100846744+ahtishamkhan5678@users.noreply.github.com>
2022-11-27 09:25:23 +0530

    Final Submission✅

---
+++ Logo/src/test/java/com/root/LogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
